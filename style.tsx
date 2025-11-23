81701032421ec635c86a5795e80536ccc58b0626
MUKUL VERMA <94351845+mukulv439@users.noreply.github.com>
2025-11-23 15:22:53 +0530

    Create style.tsx

---
+++ style.tsx
@@ -0,0 +1,31 @@
+body {
+  font-family: sans-serif;
+}
+.modal
+{
+  text-align: center;
+  z-index: 99999;
+  background: white;
+  padding: 52px;
+  margin-top: 100px;
+ 
+
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5); /* semi-transparent */
+
+}
+.cls_btn
+{
+  display: block;
+  color: white;
+  text-align: center;
+  background: #000;
+  cursor: pointer;
+  margin:10px;
+}
